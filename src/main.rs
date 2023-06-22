352ee82d7585e693f6c75d91220c5508cc158bf5
Akraticus <oebl89@gmail.com>
2023-06-22 10:14:49 +0200

    Moved tick-logic to its own plugin. Todo: run tick-logic in BeforeUpdate.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,17 @@
 use bevy::{prelude::*};
 
-#[derive(Resource, PartialEq)]
-struct WillTick(bool);
+mod tick_plugin;
+use tick_plugin::*;
 
-#[derive(Resource)]
-struct TickTimer(Timer);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(TickTimer(Timer::from_seconds(1.0, TimerMode::Repeating)))
-        .insert_resource(WillTick(false))
-        .add_system(iterate_tick_timer)
-        .add_system(update_will_tick.after(iterate_tick_timer))
-        .add_system(test_system.run_if(resource_exists_and_equals(WillTick(true))).after(update_will_tick))
+        .add_plugin(tick_plugin::TickPlugin)
+        .add_system(test_system.run_if(resource_exists_and_equals(WillTick(true))))
         .run();
 }
 
 fn test_system(time:Res<Time>){
     info!("Test System - Tick delta: {:?}", time.elapsed());
-}
-
-fn update_will_tick(tick_timer:Res<TickTimer>, mut will_tick: ResMut<WillTick>){
-    if tick_timer.0.just_finished(){
-        will_tick.0 = true;
-    }
-    else{
-        will_tick.0 = false;
-    }
-}
-
-fn iterate_tick_timer(time:Res<Time>, mut tick_timer:ResMut<TickTimer>){
-    tick_timer.0.tick(time.delta());
 }
---
+++ src/tick_plugin.rs
@@ -0,0 +1,34 @@
+use bevy::prelude::*;
+
+
+pub struct TickPlugin;
+
+impl Plugin for TickPlugin{
+    fn build(&self, app: &mut bevy::prelude::App) {
+
+        app
+            .insert_resource(TickTimer(Timer::from_seconds(1.0, TimerMode::Repeating)))
+            .insert_resource(WillTick(false))
+            .add_system(iterate_tick_timer)
+            .add_system(update_will_tick.after(iterate_tick_timer));
+    }
+}
+
+#[derive(Resource, PartialEq)]
+pub struct WillTick(pub bool);
+
+#[derive(Resource)]
+struct TickTimer(Timer);
+
+fn update_will_tick(tick_timer:Res<TickTimer>, mut will_tick: ResMut<WillTick>){
+    if tick_timer.0.just_finished(){
+        will_tick.0 = true;
+    }
+    else{
+        will_tick.0 = false;
+    }
+}
+
+fn iterate_tick_timer(time:Res<Time>, mut tick_timer:ResMut<TickTimer>){
+    tick_timer.0.tick(time.delta());
+}
